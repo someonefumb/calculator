a635eab5fd762deb495aa23c3a4d86d8f704fbf0
someone_fumb <someonefumb@gmail.com>
2023-07-18 19:09:27 -0400

    Add calculator number display

--- script.js
+++ script.js
@@ -3,39 +3,21 @@ let num1 = 0;
 let num2 = 0;
 let operator = "";
 
-const btns = document.querySelectorAll("button#number");
+const bod = document.querySelector('body')
+const output = document.createElement('p')
+bod.appendChild(output);
+
+const btns = document.querySelectorAll("button#numberBtn");
 btns.forEach(function(button) {
 	button.addEventListener('click', () => {
-		console.log(button.className);
 		if (operatorFunc === true) {
-			num1 = getNum();
+			num1 = button.textContent;
+			output.textContent += num1;
+			num1 = +output.textContent;
 		} else {
-			num2 = getNum();
-		}
-
-		function getNum() {
-			switch(button.className){
-				case "zero":
-					return "0";
-				case "one":
-					return "1";
-				case "two":
-					return "2";
-				case "three":
-					return "3";
-				case "four":
-					return "4";
-				case "five":
-					return "5";
-				case "six":
-					return "6";
-				case "seven":
-					return "7";
-				case "eight":
-					return "8";
-				case "nine":
-					return "9";
-			}
+			num2 = button.textContent;
+			output.textContent += num2;
+			num2 = +output.textContent;
 		}
 	})
 })
